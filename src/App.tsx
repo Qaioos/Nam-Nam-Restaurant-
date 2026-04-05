6190559ebb719027f8275166af62c90c5103234f
Qaioos <legend442005@gmail.com>
2026-04-05 11:58:01 +0300

    imported ./App.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import "./App.scss";
 //React Pages
 import Layout from "./Components/Layout/Layout";
 import Home from "./Pages/home/Home";
